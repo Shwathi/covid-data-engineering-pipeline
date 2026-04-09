271e4adf349c0589718ab271116c1dfa7f38c38e
Shwathi <90307596+Shwathi@users.noreply.github.com>
2026-04-09 23:59:59 +0530

    Initialize CovidAnalysis with SparkSession
    Set up initial structure for COVID data analysis using Spark.

--- scripts/spark/src/main/scala/CovidAnalysis.scala
+++ scripts/spark/src/main/scala/CovidAnalysis.scala
@@ -1,1 +1,17 @@
+import org.apache.spark.sql.SparkSession
 
+object CovidAnalysis {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder()
+      .appName("COVID Data Analysis")
+      .master("local[*]")
+      .getOrCreate()
+
+    // TODO: Load dataset
+    // TODO: Perform transformations
+    // TODO: Compute insights
+
+    spark.stop()
+  }
+}
