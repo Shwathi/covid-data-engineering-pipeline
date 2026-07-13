ff430759bf5acb5c72670c587cf98dbefce9db2b
Shwathi <shwathipsg@gmail.com>
2026-07-13 23:46:30 +0530

    Add MySQL schema and data loading script

--- scripts/mysql/create_table.sql
+++ scripts/mysql/create_table.sql
@@ -1,1 +1,29 @@
+CREATE DATABASE IF NOT EXISTS CovidDB;
 
+USE CovidDB;
+
+CREATE TABLE CovidData(
+    iso_code VARCHAR(10),
+    continent VARCHAR(100),
+    location VARCHAR(100),
+    date_current VARCHAR(100),
+    total_cases DOUBLE,
+    total_deaths DOUBLE,
+    total_vaccinations DOUBLE,
+    people_vaccinated DOUBLE,
+    median_age DOUBLE,
+    age_65_older DOUBLE,
+    age_70_older DOUBLE,
+    cardiovasc_death_rate DOUBLE,
+    diabetes_prevalence DOUBLE
+);
+
+LOAD DATA LOCAL INFILE '/home/cloudera/DataSheet.csv'
+INTO TABLE CovidData
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+SELECT COUNT(*) FROM CovidData;
+SELECT * FROM CovidData LIMIT 5;
